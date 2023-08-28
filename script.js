10d5fd4176541c34a88c3363d3685034fbee866d
Dexi <109952800+Betka-H@users.noreply.github.com>
2023-08-28 14:42:46 +0200

    yes the nl is done i think

--- script.js
+++ script.js
@@ -10,19 +10,60 @@ function toggleContent(contentId) {
 
 let nlcustomcheck;
 
-function nlcustomradiooff(){
+/* function nlcustomradiooff(){
         nlcustomcheck=false;
         console.log("custom false");
-};
+}; */
 
-function nlcustomradioon(){
+/* function nlcustomradioon(){
     nlcustomcheck=true;
     console.log("custom true");
-};
+}; */
 
 
-function subcheck(){
-    if (document.getElementById("nlmailinput").value==null){
-        console.log("a");
-    }
+
+function subalert(){
+    alert("Thank you for subscribing!\nYour data may or may not have been recorded.");
 }
+
+function toggleDiv(show) {
+    const div = document.getElementById('myDiv');
+    div.style.display = show ? 'block' : 'none';
+  }
+
+
+
+  let totalDays = 0;
+
+
+  function updateDaysint2(value) {
+    totalDays = value;
+    console.log(totalDays);
+  }
+
+
+
+/* 
+  let totalDays = 0;
+
+  function updateTotalDays() {
+    const months = parseInt(document.querySelector('input[name="nfcmonth"]').value);
+    const weeks = parseInt(document.querySelector('input[name="nfcweek"]').value);
+    const days = parseInt(document.querySelector('input[name="nfcday"]').value);
+
+    totalDays = (months * 30) + (weeks * 7) + days; // Assuming 30 days per month
+    document.getElementById('totalDays').textContent = totalDays;
+  } */
+
+
+
+ 
+                            
+                              function updateTotalDays() {
+                                const months = parseInt(document.querySelector('input[name="nfcmonth"]').value);
+                                const weeks = parseInt(document.querySelector('input[name="nfcweek"]').value);
+                                const days = parseInt(document.querySelector('input[name="nfcday"]').value);
+                            
+                                totalDays = (months * 30) + (weeks * 7) + days; // Assuming 30 days per month
+                                console.log(totalDays);
+                              }
